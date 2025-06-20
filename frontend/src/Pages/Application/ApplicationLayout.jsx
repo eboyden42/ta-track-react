e0860dbde4a29189bd96102d3383e596a0d67483
Eli Boyden <aqu4eb@virginia.edu>
2025-06-20 11:56:59 -0400

    added profile and logout dropdown

--- frontend/src/Pages/Application/ApplicationLayout.jsx
+++ frontend/src/Pages/Application/ApplicationLayout.jsx
@@ -1,4 +1,5 @@
-import { FaRegUserCircle } from "react-icons/fa";
+import { FaRegUserCircle, FaUser, FaUserAltSlash } from "react-icons/fa"
+import { IoIosExit } from "react-icons/io";
 import "./ApplicationLayout.scss"
 import { UserContext } from "../../App";
 import { useContext, useState, useEffect } from "react";
@@ -9,43 +10,48 @@ export default function ApplicationLayout() {
     const { user, setUser } = useContext(UserContext)
     const [showDropdown, setShowDropdown] = useState(false);
 
-    function handleProfileClick(e) {
+    // Navigation
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        // if user is logged in simply navigate to the user page
+        if (user) {
+            navigate("/user")
+        }
+        
+        // get user info with session cookies if possible
+        fetch(`${import.meta.env.VITE_API_URL}/api/session_check`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: "include",
+        })
+        .then(res => {
+            if (!res.ok) throw new Error("Not logged in")
+                return res.json()
+        })
+        .then(data => {
+            console.log("Session persisted...")
+            setUser(data.user)
+            navigate("/user")
+        })
+        .catch(() => console.log("User not logged in"))
+    }, [])
+    
+    function openDropdown(e) {
         e.preventDefault();
-        setShowDropdown((prev) => !prev);
+        setShowDropdown((prev) => !prev)
+        console.log(showDropdown)
+    }
+
+    function handleProfile() {
+        // add logic later
+        setShowDropdown(false)
     }
 
     function handleLogout() {
         // Add your logout logic here
-        setShowDropdown(false);
+        setShowDropdown(false)
     }
-
-    
-      // Navigation
-      const navigate = useNavigate()
-    
-      useEffect(() => {
-      // if user is logged in simply navigate to the user page
-      if (user) {
-        navigate("/user")
-      }
-    
-      // get user info with session cookies if possible
-      fetch(`${import.meta.env.VITE_API_URL}/api/session_check`, {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
-          credentials: "include",
-        })
-        .then(res => {
-          if (!res.ok) throw new Error("Not logged in")
-          return res.json()
-        })
-        .then(data => {
-          console.log("Session persisted...")
-          setUser(data.user)
-          navigate("/user")
-        })
-        .catch(() => console.log("User not logged in"))
-    }, [])
 
     return (
         <>
@@ -56,19 +62,24 @@ export default function ApplicationLayout() {
                 <NavLink to="/user/info">Info</NavLink>
             </div>
             <div className="right-tabs">
-                <NavLink className="profile" to="/user">
-                {
-                    //update this with a profile and logout dropdown
-                }
-                    {user ? user.username : "Login"}
+                <div className="profile" onClick={openDropdown} >
+                    <span>
+                        {user ? user.username : "Login"}
+                    </span>
                     <FaRegUserCircle />
                     {showDropdown && (
                         <div className="profile-dropdown">
-                            <NavLink to="/user/profile" onClick={() => setShowDropdown(false)}>Profile</NavLink>
-                            <button onClick={handleLogout}>Logout</button>
+                            <button onClick={handleProfile}>
+                                <FaUser />
+                                {" Profile"}
+                            </button>
+                            <button onClick={handleLogout}>
+                                <IoIosExit />
+                                {" Logout"}
+                            </button>
                         </div>
                     )}
-                </NavLink>
+                </div>
             </div>
         </nav>
         <Outlet />
