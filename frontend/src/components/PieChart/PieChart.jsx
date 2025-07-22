36ec0aa58bdd3199deeb0a3317ed63224d28ee24
Eli Boyden <aqu4eb@virginia.edu>
2025-07-21 20:48:50 -0400

    added better color generation for pie chart

--- frontend/src/components/PieChart/PieChart.jsx
+++ frontend/src/components/PieChart/PieChart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Pie } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 
@@ -6,7 +6,10 @@ ChartJS.register(ArcElement, Tooltip, Legend)
 
 export default function PieChart({ data, ...rest }) {
 
+    // state to manage generated colors for the pie chart
+    const [generatedColors, setGeneratedColors] = React.useState()
 
+    // function to generate random colors for the pie chart
     function generateColors(numColors) {
         const colors = []
         for (let i = 0; i < numColors; i++) {
@@ -18,8 +21,15 @@ export default function PieChart({ data, ...rest }) {
         return colors
     }
 
+    useEffect(() => {
+        // generate colors when the component mounts or data changes
+        if (data && data.values) {
+            const colors = generateColors(data.values.length)
+            setGeneratedColors(colors)
+        }
+    }, [JSON.stringify(data.values), JSON.stringify(data.labels)])
+
     // prepare data for the pie chart
-    const generatedColors = generateColors(data.values.length)
     const chartData = {
         labels: data.labels,
         datasets: [
